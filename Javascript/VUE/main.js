d2935398a183cd5f1d364e5a0eea7a3b154cbeb0
ZAshby <zanecashby@gmail.com>
2022-08-23 00:43:46 -0400

    VUE lessons

--- Javascript/VUE/main.js
+++ Javascript/VUE/main.js
@@ -5,6 +5,8 @@ Vue.createApp({
             message: 'class hedgehog',
             todoText: '',
             todoList: [],
+            nextId: 0,
+        
     }},
     // creating function based on user input such as button. Use data. or this. to access data in function
     methods: {
@@ -12,14 +14,59 @@ Vue.createApp({
             alert(this.message)
         },
         addTodo: function(){
-            this.todoList.push(this.todoText);
+            this.todoList.push({
+            text: this.todoText, 
+            id: this.nextId,
+            completed: false,
+            });
+            this.nextId +=1;
             this.todoText = '';
+            this.saveToLocal();
         },
-        removeTodo: function(todo){
-            const index = this.todoList.indexOf(todo)
-            this.todoList.splice(index, 1);
+        removeTodo: function(id){
+            // const index = todo.id
+            // this.todoList.splice(index, 1);
+            this.todoList = this.todoList.filter(todo => todo.id != id)
+            this.saveToLocal();
+        },
+        completeTodo:function(id){
+            this.todoList.forEach(todo =>{
+                if(todo.id === id){
+                    todo.completed = !todo.completed;
+                }
+            })
+            this.saveToLocal();
+        },
+        saveToLocal:function(){
+            const todoListString = JSON.stringify(this.todoList);
+            console.log(todoListString);
+            localStorage.setItem('todoItems', todoListString);
+            localStorage.setItem('nextID', this.nextId)
         }
 
+    },
+    computed: {completedTodoList:function(){
+        return this.todoList.filter(todo=> todo.completed ===true)
+    },
+    incompleteTodoList:function(){
+        return this.todoList.filter(todo=> todo.completed ===false)
+    }, percentComplete: function(){
+        return this.completedTodoList.length / this.todoList.length *100
     }
+
+    },
+    created: function(){
+        if(localStorage.getItem('todoItems')){
+            this.todoList = JSON.parse(localStorage.getItem('todoItems'));
+        } else{
+            this.todoList = [];
+        }
+        if(localStorage.getItem('nextId')){
+            this.nextId = Number(localStorage.getItem('nextId'));
+        } else {
+            this.nextId = 0;
+        }
+        console.log(this.todoList)
+    },   
 
 }).mount('#app')
