f63c41d188f97edc0ee72194e5440cf1420493cf
ZAshby <zanecashby@gmail.com>
2023-02-21 20:59:09 -0500

    Changed navigation

--- Python work/scratch_pad.py
+++ Python work/scratch_pad.py
@@ -1,17 +1,2 @@
-x=int(input())
-y=int(input())
-
-x=x%y
-print(x,y)
-
-x=x%y
-print(x,y)
-y=y%x
-
-print(y)
-
-print(2+3*5)
-print(20.12E8)
-print(1/1)
-print(1/2+3//3+4**2)
-print(2%012)
+mylist=[i for i in range(-1,2)]
+print(mylist)
